aa7f4a4c188d5666b081c01ab854f3bf19a8aad7
Amber Martinez <martinez.amber1028@gmail.com>
2023-03-27 18:46:20 -0700

    Add welcome to homepage

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import Home from './Components/Core/Home';
+
+function App() {
+  return (
+    <div className="layer">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Core/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div class="header">
+            <h1>Hi there, I'm Amber!</h1>
+            <p style={{ fontSize: 18, fontFamily: 'Helvetica' }}>I am a full-stack developer working in gaming R&D based in San Francisco.</p>
+        </div>
+    )
+}
+
+export default Home;
