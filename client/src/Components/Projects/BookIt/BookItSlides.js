4c34ab8dcf8ee9c226729420b436c408ecf7fa40
Amber Martinez <martinez.amber1028@gmail.com>
2023-04-02 13:04:44 -0700

    Fix Bootstrap carousels on projects

--- client/src/Components/Projects/BookIt/BookItSlides.js
+++ client/src/Components/Projects/BookIt/BookItSlides.js
@@ -3,29 +3,43 @@ import React from "react";
 function BookItSlides() {
 
     return (
-        <div class="container">
-            <div id="carousel-demo" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="item">
-                    <img src="https://i.imgur.com/Xfwe5m4.png" id="slideImg"></img>
+        <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1" style={{ backgroundColor: '#8a8c92' }}></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2" style={{ backgroundColor: '#8a8c92' }}></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3" style={{ backgroundColor: '#8a8c92' }}></button>
+            </div>
+            <div class="carousel-inner" style={{ borderRadius: 50, border: "1px #dcdee8 solid" }}>
+                <div class="carousel-item active" data-bs-interval="4000">
+                <img src="https://i.imgur.com/Xfwe5m4.png" class="d-block w-100" alt="..." style={{ borderRadius: 50 }}></img>
+                {/* <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div> */}
                 </div>
-                <div class="item">
-                    <img src="https://i.imgur.com/rzocRix.png" id="slideImg"></img>
+                <div class="carousel-item" data-bs-interval="4000">
+                <img src="https://i.imgur.com/rzocRix.png" class="d-block w-100" alt="..." style={{ borderRadius: 50 }}></img>
+                {/* <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Some representative placeholder content for the second slide.</p>
+                </div> */}
                 </div>
-                <div class="item active">
-                    <img src="https://i.imgur.com/M6Aipxq.png" id="slideImg"></img>
+                <div class="carousel-item" data-bs-interval="4000">
+                <img src="https://i.imgur.com/M6Aipxq.png" class="d-block w-100" alt="..." style={{ borderRadius: 50 }}></img>
+                {/* <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Some representative placeholder content for the third slide.</p>
+                </div> */}
                 </div>
             </div>
-            {/* <!--slider control for card--> */}
-            <a class="left carousel-control" href="#carousel-demo" data-slide="prev" id="leftArrow">
-                <span class="glyphicon glyphicon-chevron-left">
-            </span>
-            </a>
-            <a class="right carousel-control" href="#carousel-demo" data-slide="next" id="rightArrow">
-                <span class="glyphicon glyphicon-chevron-right">
-            </span>
-            </a>
-            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
     )
 }
