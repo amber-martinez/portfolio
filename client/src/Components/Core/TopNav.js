9d7974c9cafa3cdabb3b41bcee393acf2e5922e1
Amber Martinez <martinez.amber1028@gmail.com>
2023-04-02 13:25:15 -0700

    Fix Bootstrap navbar

--- client/src/Components/Core/TopNav.js
+++ client/src/Components/Core/TopNav.js
@@ -16,34 +16,32 @@ function TopNav() {
     )
 
     return (
-        <nav class="navbar navbar-inverse" style={{ backgroundColor: '#f9faff', border: 'none' }}>
+        <nav class="navbar navbar-expand-lg bg-light" id="topNav">
             <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>                        
-                    </button>
-                </div>
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav">
-                        <li>
-                            <a id="navLink" href="/" style={{ textDecoration: 'none' }}>Amber Martinez</a>
-                        </li>
-                        <li>
-                            <a id="navLink" href="/about-me" style={{ textDecoration: 'none' }}>About Me</a>
-                        </li>
-                        <li class="dropdown">
-                            <a id="navLink" class="dropdown-toggle" data-toggle="dropdown" href="/swiftle-wordle" style={{ backgroundColor: 'transparent', textDecoration: 'none' }}>Work<span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a href="/swiftle-wordle" style={{ textDecoration: 'none' }}>Swiftle</a></li>
-                                <li><a href="/book-it" style={{ textDecoration: 'none' }}>Book It</a></li>
-                                <li><a href="/my-booknook" style={{ textDecoration: 'none' }}>My Booknook</a></li>
-                                <li><a href="/harvardX" style={{ textDecoration: 'none' }}>HarvardX CS50x</a></li>
-                            </ul>
-                        </li>
+                <a class="navbar-brand" href="/">Amber Martinez</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="/about-me">About</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Work
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="/swiftle-wordle">Swiftle</a></li>
+                        <li><a class="dropdown-item" href="/book-it">Book It</a></li>
+                        <li><a class="dropdown-item" href="/my-booknook">My Booknook</a></li>
+                        <li><a class="dropdown-item" href="/harvardX">HarvardX CS50x</a></li>
                     </ul>
+                    </li>
+                </ul>
                 </div>
             </div>
         </nav>
