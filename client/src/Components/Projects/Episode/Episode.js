4dac066a401fffb8c16ded9c84ad86a7c5b5247d
Amber Martinez <martinez.amber1028@gmail.com>
2023-05-08 12:42:18 -0700

    Add Onboarding for Episode projects

--- client/src/Components/Projects/Episode/Episode.js
+++ client/src/Components/Projects/Episode/Episode.js
@@ -1,6 +1,6 @@
 import React from "react";
-import AvatarSpace from "./AvatarSpace.mp4"
-import WardrobeBuilder from "./WardrobeBuilder.mp4"
+import WardrobeBuilder from "./WardobeBuilder";
+import Onboarding from "./Onboarding";
 
 function Episode() {
 
@@ -16,36 +16,8 @@ function Episode() {
                     <li>Avatar Space</li> */}
             </p>
             <br></br>
-            <h3>Wardrobe Builder</h3>
-            <hr></hr>
-            <table>
-                <tr>
-                    <td style={{ verticalAlign: 3 }}>
-                        <p>
-                            This feature allows players to:
-                            <ul>
-                                <li>Build a wardrobe of outfits they can use throughout the story they're currently reading</li>
-                                <li>Purchase any clothes they had not yet added during any visit to the wardrobe</li>
-                            </ul>
-                            The key in this feature was the promise and fulfillment of purchasing an outfit that will last for the rest of the story. Plus, as we repeatedly showed them the outfits they had not yet purchased, they often converted and bought most of the outfits by the end of the story. This was planned on the basis of repeated information being perceived as more truthful.
-                            <br></br><br></br>
-                            <h4>Results</h4>
-                            <ul>
-                                <li>Increased monetization by ~25% across all chapters in a story</li>
-                                <li>Doubled monetization in first chapter of story</li>
-                            </ul>
-                            <br></br>
-                        </p>
-                    </td>
-                    <td>
-                        <video src={WardrobeBuilder} controls height="500px" width="autp" style={{ paddingLeft: 40 }}></video>
-                        <br></br>
-                    </td>
-                </tr>
-            </table>
-            <div>
-                <video src={AvatarSpace} controls height="600px" width="autp"></video>
-            </div>
+            <WardrobeBuilder/>
+            <Onboarding/>
         </div>
     )
 }
---
+++ client/src/Components/Projects/Episode/Onboarding.js
@@ -0,0 +1,37 @@
+import React from "react";
+import video from "./Onboarding.mp4"
+
+function Onboarding() {
+    return (
+        <div>
+            <h3>Interactive Onboarding Experience</h3>
+            <hr></hr>
+            <table>
+                <tr>
+                    <td style={{ verticalAlign: 3 }}>
+                        <p>
+                            This initiative:
+                            <ul>
+                                <li>Reimagines the onboarding experience (what players who just download the game first experience)</li>
+                                <li>Replaced the static welcome page with an animated carousel of stories to choose from</li>
+                            </ul>
+                            The idea was to enhance the experience a new player has when they first open the game. In order to make a better first impression, I developed an interactive and animated carousel that lets readers pick a story while already being immersed in the story. The animations and visuals are directly from the story being offered to them. We found that this also helped direct them into a story they invested more in, presumably because they had a better idea of what they were choosing.
+                            <br></br><br></br>
+                            <h4>Results</h4>
+                            <ul>
+                                <li>~2% increase in cumulative ARPU</li>
+                            </ul>
+                            <br></br>
+                        </p>
+                    </td>
+                    <td>
+                        <video src={video} controls height="500px" width="autp" style={{ paddingLeft: 40 }}></video>
+                        <br></br>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+export default Onboarding;
