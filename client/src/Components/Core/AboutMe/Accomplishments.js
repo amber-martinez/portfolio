5531139a6cfe306c1cd8d75135e2341dcc8bb8a5
Amber Martinez <martinez.amber1028@gmail.com>
2024-03-30 11:40:29 -0400

    update accomplishments

--- client/src/Components/Core/AboutMe/Accomplishments.js
+++ client/src/Components/Core/AboutMe/Accomplishments.js
@@ -4,25 +4,38 @@ function Accomplishments() {
     return (
         <div id="sb">
             <p>
-                <h3>Accomplishments</h3>
+                <h3>Experience</h3>
+                <h5>DroneDeploy</h5>
+                    <ul>
+                        <li>
+                            Picked up new tech stack: Angular, TypeScript, RxJS.
+                        </li>
+                        <li>
+                            Designed and implemented the frontend for a new filter.
+                        </li>
+                        <li>
+                            Partake in support and on-call rotations.
+                        </li>
+                    </ul>
+
                 <h5>Pocket Gems – Episode Interactive</h5>
-                <ul>
-                    <li>
-                        Designed and developed a customer-facing feature that improves monetization by 25% per story.
-                    </li>
-                    <li>
-                        Ideated/led 8 coded game initiatives that increased monetization and decreased development production time by ~323 hours.
-                    </li>
-                    <li>
-                        Created 22 course materials for teaching new users how to code.
-                    </li>
-                    <li>
-                        Debug technical issues during production, during QA via Jira, and as the designated firefighter after release.
-                    </li>
-                    <li>
-                        Conduct research for and implement new development tools to support content creation for our platform’s creators.
-                    </li>
-                </ul>
+                    <ul>
+                        <li>
+                            Designed and developed a customer-facing feature that improves monetization by 25% per story.
+                        </li>
+                        <li>
+                            Ideated/led 8 coded game initiatives that increased monetization and decreased development production time by ~323 hours.
+                        </li>
+                        <li>
+                            Created 22 course materials for teaching new users how to code.
+                        </li>
+                        <li>
+                            Debug technical issues during production, during QA via Jira, and as the designated firefighter after release.
+                        </li>
+                        <li>
+                            Conduct research for and implement new development tools to support content creation for our platform’s creators.
+                        </li>
+                    </ul>
                 <h5>Awards</h5>
                 <ul>
                     <li>SkillsUSA – Promotional Bulletin Board (State Division, 1st Place)</li>
